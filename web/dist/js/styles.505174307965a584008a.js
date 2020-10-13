f89692423a1768ce00f3cc5f829c62238bf53011
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-10-13 21:44:30 +0100

    wip build files

--- web/dist/js/styles.505174307965a584008a.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see styles.505174307965a584008a.js.LICENSE.txt */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{346:function(n,w,o){}}]);
-//# sourceMappingURL=styles.505174307965a584008a.js.map
