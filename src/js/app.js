a117d6ef1f5ef915bfc22cf88b5491c5ce02f97c
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-10-20 13:31:16 +0100

    wip address template

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,7 @@
 import( /* webpackChunkName: "styles" */ '../css/main.css');
 
 import "core-js/stable";
+import 'alpinejs';
+import './components/addresses';
 
 console.log('Loaded.');
---
+++ src/js/components/addresses.js
@@ -0,0 +1,46 @@
+window.addresses = function() {
+    return {
+        countryId: null,
+        stateId: null,
+        modelName: null,
+        stateSelectId: null,
+        stateTextId: null,
+        showStateSelect: false,
+        allStates() {
+            return window.addressStates;
+        },
+        states() {
+            console.log('stateid', this.stateId);
+            if (this.countryId && Object.keys(this.allStates()).indexOf(this.countryId) >= 0) {
+                let states = [];
+                Object.keys(this.allStates()[this.countryId]).forEach(key => {
+                    states.push({
+                        id: key,
+                        name: this.allStates()[this.countryId][key]
+                    });
+                });
+
+                return states;
+            }
+
+            return [];
+        },
+        toggleStates() {
+
+            if (this.states().length) {
+                this.stateSelectId = this.modelName + '-state';
+                this.stateTextId = '';
+                this.showStateSelect = true;
+            } else {
+                this.stateSelectId = '';
+                this.stateTextId = this.modelName + '-state';
+                this.showStateSelect = false;
+            }
+
+        },
+        onChange(ev) {
+            this.countryId = ev.target.value;
+            this.toggleStates();
+        }
+    };
+};
