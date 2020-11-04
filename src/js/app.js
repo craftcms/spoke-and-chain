3832f45a887704a84d0dcd6a64d75b395c65ce1e
Benjamin David <ben@dukt.net>
2020-11-04 10:04:04 +0100

    Improved responsive nav

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ import( /* webpackChunkName: "styles" */ '../css/main.css');
 import "core-js/stable";
 import 'alpinejs';
 import './components/addresses';
-import './components/cart-menu';
+import './components/header';
 import './components/modal';
 import './components/notifications';
 
