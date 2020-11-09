7b806ddfed45e63a699568afd3408e96a27ff091
Benjamin David <ben@dukt.net>
2020-11-09 13:47:48 +0100

    Fixed header

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -7,6 +7,46 @@ window.header = function() {
         },
         closeCartMenu() {
             this.showCartMenu = false
+        },
+
+        lastScrollValue: 0,
+        mainNavVisible: true,
+        mainNavFixed: true,
+        mainNavHeight: 0,
+
+        init() {
+            this.updateMainNavHeight()
+        },
+
+        onScroll() {
+            this.updateMainNavHeight()
+
+            const scrollValue = window.scrollY
+
+            if (scrollValue > this.lastScrollValue) {
+                // Scroll down
+                this.mainNavVisible = false
+
+                if (scrollValue > this.mainNavHeight) {
+                    this.mainNavFixed = true
+                } else {
+                    this.mainNavFixed = false
+                    this.mainNavVisible = true
+                }
+            } else {
+                // Scroll up
+                this.mainNavVisible = true
+            }
+
+            this.lastScrollValue = scrollValue
+        },
+
+        updateMainNavHeight() {
+            const $mainNav = document.getElementById('main-nav');
+            const $header = document.getElementById('header');
+
+            this.mainNavHeight = $mainNav.offsetHeight
+            $header.style.height = this.mainNavHeight + 'px'
         }
     }
 }
