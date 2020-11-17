f5fc5277ddb2921d69470f3b3e0952c48458cbcb
Nathaniel Hammond <nathanielhammond@gmail.com>
2020-11-17 16:09:14 +0000

    Remove modal console logs

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -17,7 +17,6 @@ window.modal = function() {
         url: null, // HTML content for the modal
 
         openModal: function(options) {
-            console.log('openModal', options)
             this.show = true
             this.showWrapper = true
 
@@ -32,7 +31,6 @@ window.modal = function() {
             this.url = options.url != undefined ? options.url : this.url
         },
         closeModal: function() {
-            console.log('closeModal')
             this.show = false
             setTimeout(function() {
                 this.showWrapper = false
@@ -127,42 +125,36 @@ window.modal = function() {
             ['x-transition:enter']() {
                 this.hideContent()
                 this.loadContent()
-                console.log('x-transition:enter')
                 if (this.modalType == 'slideout') {
                     return 'transform transition ease-in-out duration-500 sm:duration-700';
                 }
                 return 'transition ease-out duration-300';
             },
             ['x-transition:enter-start']() {
-                console.log('x-transition:enter-start')
                 if (this.modalType == 'slideout') {
                     return 'translate-x-full';
                 }
                 return 'opacity-0';
             },
             ['x-transition:enter-end']() {
-                console.log('x-transition:enter-end')
                 if (this.modalType == 'slideout') {
                     return 'translate-x-0';
                 }
                 return 'opacity-100';
             },
             ['x-transition:leave']() {
-                console.log('x-transition:leave')
                 if (this.modalType == 'slideout') {
                     return 'transform transition ease-in-out duration-500 sm:duration-700';
                 }
                 return 'transition ease-out duration-300';
             },
             ['x-transition:leave-start']() {
-                console.log('x-transition:leave-start')
                 if (this.modalType == 'slideout') {
                     return 'translate-x-0';
                 }
                 return 'opacity-100';
             },
             ['x-transition:leave-end']() {
-                console.log('x-transition:leave-end')
                 if (this.modalType == 'slideout') {
                     return 'translate-x-full';
                 }
