018993f03f17122eb2ee57105b9fc82fbf0e25d5
Benjamin David <ben@dukt.net>
2021-01-18 12:26:01 +0100

    Simplified fixed headers and use sticky positioning

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,10 +1,30 @@
 window.header = function() {
     return {
+        // Nav
+        showNav: false,
+        toggleNav() {
+            this.showNav = !this.showNav
+
+            const $html = document.querySelector('html')
+            const $body = document.querySelector('body')
+
+            if (this.showNav) {
+                $html.classList.add('overflow-y-scroll', 'lg:overflow-y-auto', 'h-full', 'lg:h-auto')
+                $body.classList.add('overflow-y-hidden', 'lg:overflow-y-auto', 'h-full', 'lg:h-auto')
+            } else {
+                $html.classList.remove('overflow-y-scroll', 'lg:overflow-y-auto', 'h-full', 'lg:h-auto')
+                $body.classList.remove('overflow-y-hidden', 'lg:overflow-y-auto', 'h-full', 'lg:h-auto')
+            }
+        },
+
+        // Search
         showSearch: false,
         openSearch($nextTick) {
             this.showSearch = true
             $nextTick(() => {
                 const $searchInput = document.querySelector('#search-input')
+
+                // Put the focus on the search input
                 $searchInput.focus()
 
                 // Put the cursor at the end of the text
@@ -15,87 +35,7 @@ window.header = function() {
             this.showSearch = false
         },
 
-        showNav: false,
+        // Cart
         showCartMenu: false,
-        openCartMenu() {
-            this.showCartMenu = true
-        },
-        closeCartMenu() {
-            this.showCartMenu = false
-        },
-
-        lastScrollValue: 0,
-        mainNavVisible: true,
-        mainNavFixed: true,
-        mainNavHeight: 0,
-        scrollDirection: 'down',
-        toleranceDistance: 50,
-
-        $header: document.querySelector('#header'),
-        $mainNav: document.querySelector('#main-nav'),
-
-        init() {
-            let _this = this
-            this.updateMainNavHeight()
-
-            const ro = new ResizeObserver(entries => {
-                for (let entry of entries) {
-                    this.updateMainNavHeight();
-                }
-            });
-
-            ro.observe(this.$header);
-        },
-
-        onScroll() {
-            this.updateMainNavHeight()
-
-            const scrollValue = window.scrollY
-
-            if (scrollValue > this.lastScrollValue) {
-                this.scrollDirection = 'down'
-                // Scroll down
-                this.mainNavVisible = false
-
-                if (scrollValue > this.mainNavHeight) {
-                    this.mainNavFixed = true
-                } else {
-                    this.mainNavFixed = false
-                    this.mainNavVisible = true
-                }
-            } else {
-                // Scroll up
-                this.scrollDirection = 'up'
-                this.mainNavVisible = true
-            }
-
-            this.lastScrollValue = scrollValue
-        },
-
-        hasScrolledPastHeader() {
-            // added pixels for tolerance
-            return this.lastScrollValue > 0 && this.lastScrollValue > (this.mainNavHeight + this.toleranceDistance)
-        },
-
-        updateMainNavHeight() {
-            this.mainNavHeight = this.$mainNav.offsetHeight
-            this.$header.style.height = this.mainNavHeight + 'px'
-        },
-
-        toggleNav() {
-            this.showNav = !this.showNav
-
-            if (this.showNav) {
-                document.querySelector('html').classList.add('overflow-y-scroll', 'lg:overflow-y-auto', 'h-full', 'lg:h-auto')
-                document.querySelector('body').classList.add('overflow-y-hidden', 'lg:overflow-y-auto', 'h-full', 'lg:h-auto')
-            } else {
-                document.querySelector('html').classList.remove('overflow-y-scroll', 'lg:overflow-y-auto', 'h-full', 'lg:h-auto')
-                document.querySelector('body').classList.remove('overflow-y-hidden', 'lg:overflow-y-auto', 'h-full', 'lg:h-auto')
-            }
-
-            this.$nextTick(() => {
-                this.updateMainNavHeight()
-            })
-        }
     }
 }
