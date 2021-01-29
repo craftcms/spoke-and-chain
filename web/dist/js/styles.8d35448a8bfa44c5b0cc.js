60e62aa06f0c258472e31f317e5061571e5019c7
Benjamin David <ben@dukt.net>
2021-01-29 17:52:50 +0100

    dist

--- web/dist/js/styles.8d35448a8bfa44c5b0cc.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see styles.8d35448a8bfa44c5b0cc.js.LICENSE.txt */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{354:function(n,w,o){}}]);
-//# sourceMappingURL=styles.8d35448a8bfa44c5b0cc.js.map
