09c90644e9ba3ce0273d040106b3259d4de652e9
Benjamin David <ben@dukt.net>
2021-02-02 00:08:38 +0100

    Added a test to check that a product can be removed from the cart

--- cypress/integration/front/cart.spec.js
+++ cypress/integration/front/cart.spec.js
@@ -7,4 +7,24 @@ describe('Cart', () => {
             .its('length')
             .should('be.gt', 0)
     })
+
+    it(`should be able to remove a product from the cart`, function () {
+        cy.addProductToCart()
+        cy.navigateToCart()
+
+        cy.get('div.line-item').then(($el) => {
+            // Retrieve line item ID
+            const lineItemId = $el.attr('id').substr(10)
+
+            // Make sure the the line item exists
+            cy.get('#line-item-' + lineItemId).should('exist')
+
+            // Remove the line item
+            cy.get('#remove-trigger-' + lineItemId)
+                .click()
+
+            // Check that the line item has properly been removed
+            cy.get('#line-item-' + lineItemId).should('not.exist')
+        })
+    })
 })
