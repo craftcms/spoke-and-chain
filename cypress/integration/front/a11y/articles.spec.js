8d8489ba1dc3492a7bd4e6b4cef51662470e9d3c
Benjamin David <ben@dukt.net>
2021-02-09 13:36:27 +0100

    Added article a11y audit

--- cypress/integration/front/a11y/articles.spec.js
+++ cypress/integration/front/a11y/articles.spec.js
@@ -4,3 +4,15 @@ describe('Articles', () => {
         cy.runAudit()
     })
 })
+
+describe('Articles → Article', () => {
+    it("should pass the audits", function () {
+        cy.visit('/articles')
+
+        // Click the first article
+        cy.get('a.article-card').first()
+            .click()
+
+        cy.runAudit()
+    })
+})
