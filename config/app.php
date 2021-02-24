189cfb630968fb5072b4ca8be1b0c82e45bed1ee
Nathaniel Hammond <nathaniel@pixelandtonic.com>
2021-02-24 16:44:22 +0000

    Rename custom module

--- config/app.php
+++ config/app.php
@@ -22,7 +22,7 @@ use craft\helpers\App;
 return [
     'id' => App::env('APP_ID') ?: 'CraftCMS',
     'modules' => [
-        'my-module' => \modules\Module::class,
+        'spoke' => \modules\Module::class,
     ],
-    'bootstrap' => ['my-module'],
+    'bootstrap' => ['spoke'],
 ];
