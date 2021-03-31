c63f8b3e1966d28881da4395018a31ff8d4d8ff9
Benjamin David <ben@dukt.net>
2021-03-31 16:43:59 +0200

    dist

---
+++ web/dist/js/styles.97ed12959009af61c24a.js
@@ -0,0 +1,3 @@
+/*! For license information please see styles.97ed12959009af61c24a.js.LICENSE.txt */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{358:function(n,w,o){}}]);
+//# sourceMappingURL=styles.97ed12959009af61c24a.js.map
--- web/dist/js/styles.e1c27309f0f3d5fd111f.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see styles.e1c27309f0f3d5fd111f.js.LICENSE.txt */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{358:function(n,w,o){}}]);
-//# sourceMappingURL=styles.e1c27309f0f3d5fd111f.js.map
