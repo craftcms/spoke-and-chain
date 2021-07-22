2454ee6db7b56942a2bba5ae7f3d9c49e598dd4a
Matt Stein <matt@pixelandtonic.com>
2021-07-21 19:05:08 -0500

    Cleanup.
    More consistent with Europa, easier readability where explanatory comment links seem like more of a barrier.

--- config/general.php
+++ config/general.php
@@ -5,6 +5,8 @@
  * All of your system's general configuration settings go in here. You can see a
  * list of the available settings in vendor/craftcms/cms/src/config/GeneralConfig.php.
  *
+ * https://craftcms.com/docs/3.x/config/config-settings.html
+ *
  * @see \craft\config\GeneralConfig
  */
 
@@ -13,54 +15,26 @@ use craft\helpers\App;
 return [
     // Global settings
     '*' => [
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#allowadminchanges
-        'allowAdminChanges' => true,
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#autologinafteraccountactivation
-        'autoLoginAfterAccountActivation' => true,
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#cptrigger
-        'cpTrigger' => 'admin',
-
-        // Dev Mode (see https://craftcms.com/guides/what-dev-mode-does)
-        'devMode' => false,
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#defaultweekstartday
-        'defaultWeekStartDay' => 1,
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#disallowrobots
-        'disallowRobots' => true,
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#errortemplateprefix
-        'errorTemplatePrefix' => '_errors/',
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#generatetransformsbeforepageload
-        'generateTransformsBeforePageLoad' => true,
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#loginpath
-        'loginPath' => 'account/login',
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#omitscriptnameinurls
-        'omitScriptNameInUrls' => true,
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#postloginredirect
-        'postLoginRedirect' => 'account',
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#securitykey
-        'securityKey' => App::env('SECURITY_KEY'),
-
-        // https://craftcms.com/docs/3.x/config/config-settings.html#useemailasusername
-        'useEmailAsUsername' => true,
-
         'aliases' => [
             '@web' => App::env('DEFAULT_SITE_URL'),
         ],
-
+        'allowAdminChanges' => true,
+        'autoLoginAfterAccountActivation' => true,
+        'cpTrigger' => 'admin',
         'defaultSearchTermOptions' => [
             'subLeft' => true,
             'subRight' => true,
         ],
+        'defaultWeekStartDay' => 1,
+        'devMode' => false,
+        'disallowRobots' => true,
+        'errorTemplatePrefix' => '_errors/',
+        'generateTransformsBeforePageLoad' => true,
+        'loginPath' => 'account/login',
+        'omitScriptNameInUrls' => true,
+        'postLoginRedirect' => 'account',
+        'securityKey' => App::env('SECURITY_KEY'),
+        'useEmailAsUsername' => true,
     ],
 
     // Dev environment settings
