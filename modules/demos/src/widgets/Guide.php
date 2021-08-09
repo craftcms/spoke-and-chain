912961e08ddf49ce720f03cfb77f78464acbef4a
Matt Stein <matt@pixelandtonic.com>
2021-08-09 13:45:53 -0500

    Add guide dashboard widget.

---
+++ modules/demos/src/widgets/Guide.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace modules\demos\widgets;
+
+use Craft;
+use craft\base\Widget;
+
+/**
+ * Adds a custom “Guide” widget that feels at home in the dashboard.
+ * @property-read mixed  $bodyHtml
+ * @property-read string $title
+ */
+class Guide extends Widget
+{
+    /**
+     * @inheritdoc
+     */
+    public static function displayName(): string
+    {
+        return Craft::t('app', 'Guide');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected static function allowMultipleInstances(): bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function icon()
+    {
+        return Craft::getAlias('@appicons/book.svg');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getTitle(): string
+    {
+        return '';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getBodyHtml()
+    {
+        $view = Craft::$app->getView();
+        $iconsDir = Craft::getAlias('@appicons');
+
+        return $view->renderTemplate('modules/widgets/guide.twig', [
+            'bookIcon' => file_get_contents($iconsDir . '/book.svg')
+        ]);
+    }
+}
