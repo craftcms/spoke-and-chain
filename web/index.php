31655e139e1556cbb3ceb0a55f2f0d7be2e4da8c
Piotr <piotrpog@protonmail.com>
2022-08-31 12:29:47 +0200

    latest dotenv

--- web/index.php
+++ web/index.php
@@ -3,21 +3,10 @@
  * Craft web bootstrap file
  */
 
-// Set path constants
-define('CRAFT_BASE_PATH', dirname(__DIR__));
-define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
-
-// Load Composer's autoloader
-require_once CRAFT_VENDOR_PATH.'/autoload.php';
-
-// Load dotenv?
-if (class_exists('Dotenv\Dotenv') && file_exists(CRAFT_BASE_PATH.'/.env')) {
-    Dotenv\Dotenv::create(CRAFT_BASE_PATH)->load();
-}
-
-define('CRAFT_ENVIRONMENT', getenv('CRAFT_ENVIRONMENT') ?: 'production');
+// Load shared bootstrap
+require dirname(__DIR__) . '/bootstrap.php';
 
 // Load and run Craft
 /** @var craft\web\Application $app */
-$app = require CRAFT_VENDOR_PATH.'/craftcms/cms/bootstrap/web.php';
+$app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
 $app->run();
