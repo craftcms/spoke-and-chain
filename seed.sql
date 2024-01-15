89dead06faa9e9b3781350c55ad315e3da91031b
Tim Kelty <tim@craftcms.com>
2024-01-15 12:34:17 -0500

    New seed

--- seed.sql
+++ seed.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f19423cc54b668d8e12735f88e3a915ee2a969d408f2e2ea1d10647228386a8
-size 150493298
+oid sha256:aff79c700e60d07f4ba58173bae59b879d7871faf3db2662ba9a02333fbbef1a
+size 83820221
