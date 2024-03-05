6d406a5b069ffc04652fc3f0939f6222caa786d6
Tim Kelty <tim@craftcms.com>
2024-03-05 10:17:14 -0500

    fix asset

--- seed.sql
+++ seed.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1ad6f72073aa3124aa599e6d79b15eccb1e1bd405f5c61280b5a416b6519b86
-size 81789471
+oid sha256:1c07c82db4b4eaeb04b0d76fd5c5ff667d5189e070ec2bf0f2a34fab49ef6f9e
+size 81818769
