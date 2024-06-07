202a0f92253319cb518693ee017b0e9c2bd3be2e
Tim Kelty <tim@craftcms.com>
2024-06-07 13:06:02 -0400

    Use non-maria dump

--- seed.sql
+++ seed.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69d0f94dae658e1ec662d10cf185c330f7289e9638d0821b7d9d45574107ba7c
-size 88083276
+oid sha256:2e950562c73eaadbb2f2be2ecac99337a6ae1c3655f617c6f9beb7305f7a38dd
+size 87481935
